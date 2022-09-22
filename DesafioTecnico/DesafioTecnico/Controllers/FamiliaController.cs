89d5f82786e39877ad6a691779fb6c42204d4d1b
joaocarlosdv <joaocarlosdv@hotmail.com>
2022-09-22 15:32:29 -0300

    Ajustes

--- DesafioTecnico/DesafioTecnico/Controllers/FamiliaController.cs
+++ DesafioTecnico/DesafioTecnico/Controllers/FamiliaController.cs
@@ -1,10 +1,7 @@
 ﻿using DesafioTecnico.Models;
 using DesafioTecnico.Services;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DesafioTecnico.Controllers
 {
